db2cce888bd13c98b58d8ef94d6dab9496431a84
unknown <dawschae@gmail.com>
2020-06-14 15:25:17 +0900

    login API

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,27 +2,30 @@
 // member.js
 const express = require("express");
 const db = require("../connect");
-
+const base64 =  require("js-base64").Base64;
 const router = express.Router();
 
 router.post("/", (req, res) => {
   const {userId, password} = req.body;
-console.log(res.status(200));
-
-
 
   // db select문 수행
   db((err, connection) => {
 
-
-    connection.query("SELECT * FROM USER WHERE USER_ID=?",[userId], (err, rows) => {
+    connection.query("SELECT USER_ID, NAME FROM USER WHERE USER_ID=? AND PASSWORD = ?",[userId, base64.encode(password)], (err, rows) => {
       connection.release(); // 연결세션 반환.
       if (err) {
         throw err;
       }
-      console.log(rows)
-      console.log(rows[0]);
-      return res.json({ status : 200 , data: rows[0] }); // 결과는 rows에 담아 전송
+      const userInfo = rows[0]||{};
+      var jsonData = {
+          status : "200"
+        , data   : {
+            auth :  rows.length > 0 ,
+            userInfo : userInfo
+        }
+      }
+
+      return res.json(jsonData); // 결과는 rows에 담아 전송
     });
   });
 });
--- src/modules/user.js
+++ src/modules/user.js
@@ -5,9 +5,7 @@ import createRequestSaga, {createRequestActionTypes} from '../lib/createRequestS
 
 const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
 // 회원 정보 확인
-const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
-  'user/CHECK',
-);
+const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes('user/CHECK');
 const LOGOUT = 'user/LOGOUT';
 
 export const tempSetUser = createAction(TEMP_SET_USER, user => user);
